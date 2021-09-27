5c7ad42bfeef94f5acaec7bacb3a6185d95074df
Aleksey Vishniak <aleksey.vishniak@skywindgroup.com>
2021-09-27 18:45:02 +0300

    add greed

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,29 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">Hello
+    <div className="App">
+      <div className="Greed">
+        <svg width="230" height="140">
+          <rect x="0" y="0" width="230" height="140"
+            fill="white" stroke="steelblue" stroke-width="1"
+          />
+          <rect x="0" y="0" width="30" height="30"
+            fill="skyblue" stroke="steelblue" stroke-width="1"
+          />
+          <rect x="30" y="0" width="30" height="30"
+            fill="skyblue" stroke="steelblue" stroke-width="1"
+          />
+          <rect x="60" y="0" width="30" height="30"
+            fill="skyblue" stroke="steelblue" stroke-width="1"
+          />
+          <rect x="90" y="0" width="30" height="30"
+            fill="skyblue" stroke="steelblue" stroke-width="1"
+          />
+          <rect x="30" y="0" width="30" height="30"
+            fill="skyblue" stroke="steelblue" stroke-width="1"
+          />
+        </svg>
+      </div>
     </div>
   );
 }
