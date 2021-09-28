ef0df89f3fe4619af50f06776a5d4c18c82a747d
Aleksey Vishniak <aleksey.vishniak@skywindgroup.com>
2021-09-28 21:24:15 +0300

    add first 10 cells

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
 import React from 'react';
 import './App.scss';
 
+
+let coordinates = [[0, 0], [50, 0], [100, 0], [150, 0], [200, 0], [250, 0], [300, 0], [350, 0], [400, 0], [450, 0], [500, 0], [550, 0], [600, 0]]
 function App() {
   return (
     <div className="App">
       <div className="Greed">
-        <svg width="230" height="140">
-          <rect x="0" y="0" width="230" height="140"
-            fill="white" stroke="steelblue" stroke-width="1"
-          />
-          <rect x="0" y="0" width="30" height="30"
-            fill="skyblue" stroke="steelblue" stroke-width="1"
-          />
-          <rect x="30" y="0" width="30" height="30"
-            fill="skyblue" stroke="steelblue" stroke-width="1"
-          />
-          <rect x="60" y="0" width="30" height="30"
-            fill="skyblue" stroke="steelblue" stroke-width="1"
-          />
-          <rect x="90" y="0" width="30" height="30"
-            fill="skyblue" stroke="steelblue" stroke-width="1"
-          />
-          <rect x="30" y="0" width="30" height="30"
-            fill="skyblue" stroke="steelblue" stroke-width="1"
-          />
-        </svg>
+        <SquareСell />
       </div>
     </div>
   );
 }
 
-export default App;
+export default App
+
+
+let SquareСell = () => {
+  return (
+    <div>
+      <svg className="SVG" width="600" height="150">
+        {coordinates.map((el) => {
+          return <rect x={el[0]} y={el[1]} width="50" height="50"
+            fill="skyblue" stroke="steelblue" strokeWidth="1"
+          />
+        })}
+      </svg>
+    </div>
+  )
+}
