249e8b67856fc130b9a8717eb48790b6a2775e8a
Aleksey Vishniak <aleksey.vishniak@skywindgroup.com>
2021-10-02 16:32:13 +0300

    refactor app

--- src/App.tsx
+++ src/App.tsx
@@ -4,39 +4,42 @@ import './App.scss';
 let greedWidth: number = 600
 let greedHeight: number = 150
 let numberOfSquareCells: number = 36
-let numberOfRows = 3
-let coordinatesOfFirstSquareCell: Array<number> = [0, 100];
+let numberOfRows: number = 3
 let coordinatesOfSquareCells: Array<coordinatesOfSquareCell> = []
 type coordinatesOfSquareCell = Array<number>
+let coordinatesOfFirstSquareCell: Array<number> = [0, 100];
 let oneCellWidth: number = greedWidth / 12
 let currentCoordinatesOfSquareCell: Array<number> = coordinatesOfFirstSquareCell
 let currentNumberOfRows: number = 0;
 
-function addCoordinatesOfFirstSquareCell() {
+function addCoordinatesOfFirstSquareCell(): void {
   coordinatesOfSquareCells.push(coordinatesOfFirstSquareCell)
   currentCoordinatesOfSquareCell = coordinatesOfFirstSquareCell
   currentNumberOfRows++
 }
 addCoordinatesOfFirstSquareCell()
 
-for (let i = 1; i < numberOfSquareCells; i++) {
-  if (currentNumberOfRows < numberOfRows) {
-    let coordinatesOfNextCell = [currentCoordinatesOfSquareCell[0], currentCoordinatesOfSquareCell[1] - oneCellWidth]
-    coordinatesOfSquareCells.push(coordinatesOfNextCell)
-    currentCoordinatesOfSquareCell = coordinatesOfNextCell
-    currentNumberOfRows++
-  }
-  if (currentNumberOfRows >= numberOfRows) {
-    currentCoordinatesOfSquareCell = [currentCoordinatesOfSquareCell[0] + oneCellWidth, greedHeight]
-    currentNumberOfRows = 0
+function generateSquareCellsCoordinates(): void {
+  for (let i = 1; i < numberOfSquareCells; i++) {
+    if (currentNumberOfRows < numberOfRows) {
+      let coordinatesOfNextCell = [currentCoordinatesOfSquareCell[0], currentCoordinatesOfSquareCell[1] - oneCellWidth]
+      coordinatesOfSquareCells.push(coordinatesOfNextCell)
+      currentCoordinatesOfSquareCell = coordinatesOfNextCell
+      currentNumberOfRows++
+    }
+    if (currentNumberOfRows >= numberOfRows) {
+      currentCoordinatesOfSquareCell = [currentCoordinatesOfSquareCell[0] + oneCellWidth, greedHeight]
+      currentNumberOfRows = 0
+    }
   }
 }
+generateSquareCellsCoordinates()
 
 function App() {
   return (
     <div className="App">
       <div className="Greed">
-        <SquareСell />
+        <SquareСells/>
       </div>
     </div>
   );
@@ -44,16 +47,17 @@ function App() {
 
 export default App
 
-let SquareСell = () => {
+let SquareСells = () => {
   return (
     <div>
       <svg className="SVG" width="600" height="150">
         {coordinatesOfSquareCells.map((squareCell, index) => {
-          return  <g>
-            <rect x={squareCell[0]} y={squareCell[1]} key={index} width="50" height="50"
-            fill="skyblue" stroke="steelblue" strokeWidth="1"
+          let [coordX, coordY] = squareCell
+          return <g>
+            <rect x={coordX} y={coordY} key={index} width="50" height="50"
+              className="squereCell"
             ></rect>
-            <text x={squareCell[0]+18} y={squareCell[1]+30} font-size="15px" font-weight="bold" stroke="black" stroke-width="0.5" >{++index}</text>
+            <text className="squareCellText" x={coordX + 18} y={coordY + 30}>{++index}</text>
           </g>
         })}
       </svg>
