042c19e90179c3e51c32e5caacb9a5e9544fc4de
Aleksey Vishniak <aleksey.vishniak@skywindgroup.com>
2021-11-09 00:26:56 +0300

    Update bettingGrid.tsx

--- src/components/bettingGrid/bettingGrid.tsx
+++ src/components/bettingGrid/bettingGrid.tsx
@@ -4,55 +4,59 @@ import { bottomCellsCoords, dozensCoords, straightsCoords, bottomCellWidth, doze
 import { DozensText, BottomCellsText } from './constans'
 
 let BettingGrid = () => {
+    let getTextCoords = (x: number, y: number, width: number, height: number) => {
+        let [textX, textY] = [x + width / 2, y + height / 2]
+        return [textX, textY]
+    }
     return (
         <div>
             <svg className="betting-grid" >
                 {straightsCoords.map((coords, idx) => {
                     let [x, y] = coords
-                    let [textX, textY] = [x + straightWidth / 2, y + straightHeight / 1.9]
+                    let [textX, textY] = getTextCoords(x, y, straightWidth, straightHeight)
                     return <g key={idx} className="grid-cell">
                         <rect className="straight" width={straightWidth} height={straightHeight} x={x} y={y}></rect>
-                        <text className="cell-text" x={textX} y={textY}>{++idx}</text>
+                        <text className="cell-text" x={textX} y={textY} dy="3">{++idx}</text>
                     </g>
                 })}
                 {dozensCoords.map((coords, idx) => {
                     let [x, y] = coords
-                    let [textX, textY] = [x + dozenWidth / 2, y + dozenHeight / 1.8]
+                    let [textX, textY] = getTextCoords(x, y, dozenWidth, dozenHeight)
                     return <g key={idx} className="grid-cell">
                         <g>
                             <rect className='dozen-cell' width={dozenWidth} height={dozenHeight} x={x} y={y}></rect>
-                            <text className="cell-text" x={textX} y={textY}>{DozensText[idx]}</text>
+                            <text className="cell-text" x={textX} y={textY} dy="3">{DozensText[idx]}</text>
                         </g>
                     </g>
                 })}
                 {bottomCellsCoords.map((coords, idx) => {
                     let [x, y] = coords
-                    let [textX, textY] = [x + bottomCellWidth / 2, y + bottomCellHeight / 1.7]
+                    let [textX, textY] = getTextCoords(x, y, bottomCellWidth, bottomCellHeight)
                     return <g key={idx} className="grid-cell">
                         <g>
                             <rect className="bottom-cell" width={bottomCellWidth} height={bottomCellHeight} x={x} y={y}></rect>
-                            <text className="cell-text" x={textX} y={textY}>{BottomCellsText[idx]}</text>
+                            <text className="cell-text" x={textX} y={textY} dy="4">{BottomCellsText[idx]}</text>
                         </g>
                     </g>
                 })}
                 {zeroCoords.map((coords, idx) => {
                     let [x, y] = coords
-                    let [textX, textY] = [x + zeroWidth / 1.9, y + zeroHeight / 1.97]
+                    let [textX, textY] = getTextCoords(x, y, zeroWidth, zeroHeight)
                     return <g key={idx} className="grid-cell">
                         <g>
                             <path className="zero-cell" d={zeroPath} x={x} y={y}></path>
-                            <text className="cell-text" x={textX} y={textY}>0</text>
+                            <text className="cell-text" x={textX} y={textY} dy="3">0</text>
                         </g>
                     </g>
                 })}
                 {columnsCoords.map((coords, idx) => {
                     let [x, y] = coords
-                    let [textX, textY] = [x + columnWidth / 2, y + columnHeight / 1.9]
+                    let [textX, textY] = getTextCoords(x, y, columnWidth, columnHeight)
                     let transform = `rotate(-90 ${x + columnWidth / 2} ${y + columnHeight / 2})`
                     return <g key={idx} className="grid-cell">
                         <g>
                             <rect className="column-cell" width={columnWidth} height={columnHeight} x={x} y={y}></rect>
-                            <text className="cell-text" transform={transform} x={textX} y={textY}>2to1</text>
+                            <text className="cell-text" transform={transform} x={textX} y={textY} dy="3">2to1</text>
                         </g>
                     </g>
                 })}
